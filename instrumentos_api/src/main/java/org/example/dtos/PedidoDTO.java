6bf81eae3b4727d9741e00e032ef974fb9258642
Ambar Gorgon <ambargorgon@gmail.com>
2025-05-12 20:40:27 -0300

    arreglos del tp6 9/5

--- instrumentos_api/src/main/java/org/example/dtos/PedidoDTO.java
+++ instrumentos_api/src/main/java/org/example/dtos/PedidoDTO.java
@@ -1,8 +1,12 @@
 package org.example.dtos;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public record PedidoRequestDTO(
+        Long id,
+        Double totalPedido,
+        LocalDate fechaPedido,
         List<PedidoDetalleRequestDTO> pedidoDetalle
 ) {
 }
